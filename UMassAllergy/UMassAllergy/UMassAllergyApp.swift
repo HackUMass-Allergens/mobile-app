6c10dead021c47f84ce895c0c4968b9c9e4ea966
anothergenz <johnsmithcoder7@gmail.com>
2022-11-11 23:38:50 -0500

    initial app commit

---
+++ UMassAllergy/UMassAllergy/UMassAllergyApp.swift
@@ -0,0 +1,17 @@
+//
+//  UMassAllergyApp.swift
+//  UMassAllergy
+//
+//  Created by Matthew Kotchkin on 11/11/22.
+//
+
+import SwiftUI
+
+@main
+struct UMassAllergyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
