da60d95bb4974354c7bc14558b77f61057d47bf2
Donald Winkelman <dmwink.tx@gmail.com>
2022-11-12 15:17:07 -0500

    Creating homepage and 4 main pages

--- UMassAllergy/UMassAllergy/SessionView.swift
+++ UMassAllergy/UMassAllergy/SessionView.swift
@@ -13,49 +13,59 @@ import PostgREST
 struct SessionView: View {
     @Environment(\.supaClient) private var client
     public var session: Session
-    @State private var orders: [Order]?
-    @State private var locations: [Location]?
-    @State private var user: User?
-    @State private var mealPeriods: [MealPeriod]?
-    @State private var mealCategories: [MealCategory]?
-    @State private var foodGroups: [FoodGroup]?
     @State private var error: Error?
     
     var body: some View {
-        ScrollView {
-            Text(stringfy(self.foodGroups?[0]))
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding()
-            
-            if let error {
-                Section {
-                    Text(error.localizedDescription)
-                        .foregroundColor(.red)
-                }
+        NavigationView {
+            VStack {
+                NavigationLink(destination: PlaceOrder(), label: {Text("Place a New Order")})
+                NavigationLink(destination: ViewOrders(), label: {Text("View Existing Orders")})
+                NavigationLink(destination: Sett(), label: {Text("Settings")})
+                NavigationLink(destination: HelpInfo(), label: {Text("Help/Info")})
             }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.black)
         }
-        .navigationTitle("Session")
-        .toolbar {
-            ToolbarItem {
-                Button("Sign out") {
-                    Task {
-                        try? await client.auth.signOut()
-                    }
-                }
-            }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.black)
+    }
+}
+struct PlaceOrder: View {
+    var body: some View {
+        VStack {
+            label: do {Text("Place an Order") }
         }
-        .task {
-            do {
-                locations = try await getLocations(client: self.client.database)
-                orders = try await getOrders(client: self.client.database, userId: session.user.id)
-                user = try await getUser(client: self.client.database, userId: self.client.auth.session!.user.id.uuidString)
-                mealPeriods = try await getMealPeriodsAfter(client: self.client.database, date: Date.now)
-                mealCategories = try await getMealCategoriesForMealPeriod(client: self.client.database, mealPeriod: mealPeriods![0])
-                foodGroups = try await getFoodGroupsForMealCategory(client: self.client.database, mealCategory: mealCategories![0])
-            } catch {
-                self.error = error
-            }
-            
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.black)
+    }
+}
+
+struct ViewOrders: View {
+    var body: some View {
+        VStack {
+            label: do {Text("Your Existing Orders") }
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.black)
+    }
+}
+
+struct Sett: View {
+    var body: some View {
+        VStack {
+            label: do {Text("Settings") }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.black)
+    }
+}
+
+struct HelpInfo: View {
+    var body: some View {
+        VStack {
+        label: do {Text("Some Info") }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.black)
     }
 }
