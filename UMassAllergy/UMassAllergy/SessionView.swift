84e030d4cd5d6b6c08826453385ffab53c13356c
Isaac Heitmann <isaac.heitmann@gmail.com>
2022-11-12 21:13:28 -0500

    Auto stash before merge of "main" and "origin/main"

--- UMassAllergy/UMassAllergy/SessionView.swift
+++ UMassAllergy/UMassAllergy/SessionView.swift
@@ -9,6 +9,7 @@ import Foundation
 import GoTrue
 import SwiftUI
 import PostgREST
+import UIKit
 
 struct SessionView: View {
     @Environment(\.supaClient) private var client
@@ -35,10 +36,157 @@ struct SessionView: View {
         .background(Color.black)
     }
 }
+
+//: Datetime!
+let date = Date()
+let calendar  = Calendar.current
+let hour = calendar.component(.hour, from: date)
+let weekd = calendar.component(.weekday, from:date)
+var weekList = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+var weeksOne = Array(weekList[weekd-1..<7]) + Array(weekList[0..<weekd-1])
+var weeksTwo = Array(weekList[weekd..<7]) + Array(weekList[0..<weekd])
+struct dy:Identifiable {
+    var id: String
+    var meal: String
+}
+let meals = [
+    dy(id: "a", meal: "Breakfast"),
+    dy(id: "b", meal: "Lunch"),
+    dy(id: "c", meal:  "Dinner")
+]
+
 struct PlaceOrder: View {
+    @Environment (\.supaClient) private var client
+    @State private var mealPeriods: [MealPeriod]?
+    
     var body: some View {
-        VStack {
-            label: do {Text("Place Order").foregroundColor(.white)}
+        NavigationView {
+            VStack {
+                if hour < 17 {
+                    ScrollView (.horizontal) {
+                        HStack (spacing : 20) {
+                            ForEach(meals) { meal in
+                                NavigationLink(destination: Menu(day: weeksOne[0], meal: meal.meal), label: {Text(weeksOne[0] + " " + meal.meal)})
+                            }
+                        }
+                    }
+                    ScrollView (.horizontal) {
+                        HStack (spacing : 20) {
+                            ForEach(meals) { meal in
+                                NavigationLink(destination: Menu(day: weeksOne[1], meal: meal.meal), label: {Text(weeksOne[1] + " " + meal.meal)})
+                            }
+                        }
+                    }
+                    
+                    ScrollView (.horizontal) {
+                        HStack (spacing : 20) {
+                            ForEach(meals) { meal in
+                                NavigationLink(destination: Menu(day: weeksOne[2], meal: meal.meal), label: {Text(weeksOne[2] + " " + meal.meal)})
+                            }
+                        }
+                    }
+                    
+                    
+                    ScrollView (.horizontal) {
+                        HStack (spacing : 20) {
+                            ForEach(meals) { meal in
+                                NavigationLink(destination: Menu(day: weeksOne[3], meal: meal.meal), label: {Text(weeksOne[3] + " " + meal.meal)})
+                            }
+                        }
+                    }
+                    
+                    ScrollView (.horizontal) {
+                        HStack (spacing : 20) {
+                            ForEach(meals) { meal in
+                                NavigationLink(destination: Menu(day: weeksOne[4], meal: meal.meal), label: {Text(weeksOne[4] + " " + meal.meal)})
+                            }
+                        }
+                    }
+                    
+                    ScrollView (.horizontal) {
+                        HStack (spacing : 20) {
+                            ForEach(meals) { meal in
+                                NavigationLink(destination: Menu(day: weeksOne[5], meal: meal.meal), label: {Text(weeksOne[5] + " " + meal.meal)})
+                            }
+                        }
+                    }
+                    
+                    ScrollView (.horizontal) {
+                        HStack (spacing : 20) {
+                            ForEach(meals) { meal in
+                                NavigationLink(destination: Menu(day: weeksOne[6], meal: meal.meal), label: {Text(weeksOne[6] + " " + meal.meal)})
+                            }
+                        }
+                    }
+                    
+                }
+                else {
+                    ScrollView (.horizontal) {
+                        HStack (spacing : 20) {
+                            ForEach(meals) { meal in
+                                NavigationLink(destination: Menu(day: weeksTwo[0], meal: meal.meal), label: {Text(weeksTwo[0] + " " + meal.meal)})
+                            }
+                        }
+                    }
+                    ScrollView (.horizontal) {
+                        HStack (spacing : 20) {
+                            ForEach(meals) { meal in
+                                NavigationLink(destination: Menu(day: weeksTwo[1], meal: meal.meal), label: {Text(weeksTwo[1] + " " + meal.meal)})
+                            }
+                        }
+                    }
+                    
+                    ScrollView (.horizontal) {
+                        HStack (spacing : 20) {
+                            ForEach(meals) { meal in
+                                NavigationLink(destination: Menu(day: weeksTwo[2], meal: meal.meal), label: {Text(weeksTwo[2] + " " + meal.meal)})
+                            }
+                        }
+                    }
+                    
+                    
+                    ScrollView (.horizontal) {
+                        HStack (spacing : 20) {
+                            ForEach(meals) { meal in
+                                NavigationLink(destination: Menu(day: weeksTwo[3], meal: meal.meal), label: {Text(weeksTwo[3] + " " + meal.meal)})
+                            }
+                        }
+                    }
+                    
+                    ScrollView (.horizontal) {
+                        HStack (spacing : 20) {
+                            ForEach(meals) { meal in
+                                NavigationLink(destination: Menu(day: weeksTwo[4], meal: meal.meal), label: {Text(weeksTwo[4] + " " + meal.meal)})
+                            }
+                        }
+                    }
+                    
+                    ScrollView (.horizontal) {
+                        HStack (spacing : 20) {
+                            ForEach(meals) { meal in
+                                NavigationLink(destination: Menu(day: weeksTwo[5], meal: meal.meal), label: {Text(weeksTwo[5] + " " + meal.meal)})
+                            }
+                        }
+                    }
+                    
+                    ScrollView (.horizontal) {
+                        HStack (spacing : 20) {
+                            ForEach(meals) { meal in
+                                NavigationLink(destination: Menu(day: weeksTwo[6], meal: meal.meal), label: {Text(weeksTwo[6] + " " + meal.meal)})
+                            }
+                        }
+                    }
+                }
+            }
+            .frame(maxWidth: 250, maxHeight: 600)
+            .background(Color.white)
+        }
+        .task {
+            do {
+                self.mealPeriods = try await getMealPeriodsOn(client: self.client.database, date: date)
+            } catch {
+                
+            }
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color.black)
@@ -74,6 +222,19 @@ struct HelpInfo: View {
         .background(Color.black)
     }
 }
+
+struct Menu: View {
+    public var day: String
+    public var meal: String
+    var body: some View {
+        VStack {
+        label: do {Text(day + meal) }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.black)
+    }
+}
+
 
 struct ButtonView: View {
     var text:String = ""
